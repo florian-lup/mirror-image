3c08f8a4ad94316b7a6a59e5ceec1953db4a1e69
florian.lup <contact@florianlup.com>
2025-01-26 00:56:46 +0000

    Reorder PageHeader navigation items: Swap HelpPopover and ContactPopover positions

--- src/components/WelcomePage/components/PageHeader.tsx
+++ src/components/WelcomePage/components/PageHeader.tsx
@@ -5,8 +5,8 @@ export const PageHeader = () => {
   return (
     <header className="h-20 sticky top-0 z-10 backdrop-blur-sm flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/0">
       <nav className="flex justify-end items-center space-x-3 w-full px-8">
-        <HelpPopover />
         <ContactPopover />
+        <HelpPopover />
       </nav>
     </header>
   );
