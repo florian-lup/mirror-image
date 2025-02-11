f0ad312a4dced42aa0d7b8cf034dd75731acc054
florian.lup <contact@florianlup.com>
2025-02-11 14:36:06 +0000

    Remove unused tools and dependencies related to research and web search

--- packages/frontend/src/app/api/tools/tools.ts
+++
@@ -1,9 +0,0 @@
-import { KnowledgeBase } from './KnowledgeBase';
-import { DeepResearch } from './DeepResearch';
-import { WebSearch } from './WebSearch';
-
-export const tools = [
-  KnowledgeBase.create(),
-  DeepResearch.create(),
-  WebSearch.create()
-]; 
