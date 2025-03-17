d955b228888595525198b5698e2607d81fba9d41
florian-lup <contact@florianup.com>
2025-03-17 10:03:41 +0000

    moved viewport out of metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
@@ -13,17 +13,18 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+};
+
 export const metadata: Metadata = {
   title: "Florian Lup",
   description: "A dynamic AI-powered personal website that utilizes Retrieval-Augmented Generation (RAG) to deliver instant, precise answers about my background, projects, and expertise.",
   icons: {
     icon: "/favicon.ico",
-  },
-  viewport: {
-    width: "device-width",
-    initialScale: 1,
-    maximumScale: 1,
-    userScalable: false,
   },
 };
 
